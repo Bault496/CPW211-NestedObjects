41b4e0d0de1b530f21d273a1deb54f7fa92221e5
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-01-18 00:14:18 -0800

    Added Test code

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new Advisor()
+{
+    Email = "Annemarie@cptc.edu",
+    FullName = "Annemarie Smith",
+    OfficeLocation = "817 Rm 150"
+};
+
+Student student1 = new Student()
+{
+    FirstName = "Homer",
+    LastName = "Simpson",
+    DateOfBirth = new DateOnly(2000, 3, 20),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "5555554781",
+    SchoolEmail = "Homer.Simpson@students.cptc.edu"
+};
+
+Console.WriteLine($"Student: {student1.FirstName} has {student1.AssignedAdvisor.FullName}");
