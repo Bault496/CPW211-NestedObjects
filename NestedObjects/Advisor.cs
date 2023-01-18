eac805b0e8bce69ced9a8f62a8dfb627dcec8e9e
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-01-17 23:58:53 -0800

    Created student and advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Represents a college advisor
+/// </summary>
+namespace NestedObjects
+{
+    public class Advisor
+    {
+        /// <summary>
+        /// The legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// the advisors work email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Building and room number of office
+        /// </summary>
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represetns a individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The legal First Name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// The legal Last Name
+        /// </summary>
+        public string LastName { get; set; } 
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// Student's Cell Number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Studnet's Assigned Advisor
+        /// </summary>
+        public Advisor AssignedAdvisor { get; set; }
+
+    }
+}
